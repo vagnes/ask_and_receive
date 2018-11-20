c462321e591face941329c5f5e57ec24852cf21e
Kent Martin <kentaasvang@gmail.com>
2018-11-20 17:51:49 +0100

    send_recieve-function now works with front-end

--- interactive.py
+++ interactive.py
@@ -2,8 +2,7 @@ import requests
 import json
 
 
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 
 
 MENU_TEXT = """
@@ -21,16 +20,16 @@ Choose the following options:
 class Communicator(object):
 
     @staticmethod
-    def send_recieve(self, to_send):
+    def send_recieve(to_send):
         post_request = requests.post(
             "http://127.0.0.1:5000/ask/", json=to_send)
 
         # fetch post-response as json and print to terminal
         response = post_request.json()
-        print(response["processed_entry"])
+        return response["processed_entry"]
 
     @staticmethod
-    def read_last(self):
+    def read_last():
         post_request = requests.get("http://127.0.0.1:5000/last/")
 
         # fetch post-response as json and print to terminal
@@ -78,9 +77,14 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    output = None
+    if request.method == "POST":
+        entry = request.form["aar_input"]
+        entry = {"entry_string": entry}
+        output = Communicator.send_recieve(entry)
+        return render_template("index.html", output=output)
+    return render_template("index.html", output=output)
 
 
-#if __name__ == "__main__":
-    #main()
-
+if __name__ == "__main__":
+    app.run(port=5003)
