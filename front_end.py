4c4f84ed214c46f5def58faf1d0f80183bd55459
vagnes <waagnes@outlook.com>
2018-11-20 18:43:05 +0100

    interactive.py renamed front_end.py; general cleanup

--- front_end.py
+++ front_end.py
@@ -5,18 +5,6 @@ import json
 from flask import Flask, render_template, request
 
 
-MENU_TEXT = """
-
-Choose the following options:
-
-[1] : Convert text one time.
-[2] : Convert text in a continous stream.
-[3] : Check the last saved entry.
-
-[0] : Exit.
-\n> """
-
-
 class Communicator(object):
 
     @staticmethod
@@ -37,39 +25,6 @@ class Communicator(object):
         original_entry = response["entry_string"]
         processed_entry = response["processed_entry"]
         print(f"{original_entry} -> {processed_entry}")
-
-
-#def main():
-#    options = input(MENU_TEXT)
-#    c = Communicator()
-#
-#    if options == "1":
-#        to_send = input("Type what you want to send\n> ")
-#
-#        # turn input into dict to pass through request.post() as json
-#        # conversion between dict and json is automatic 
-#        to_send  = {"entry_string": to_send}
-#        c.send_recieve(to_send)
-#    elif options == "2":
-#        print("Type what you want to send. \nType 'q' to quit.")
-#
-#        while True:
-#            to_send = input("\n> ")
-#
-#            if to_send == "q":
-#                quit()
-#
-#            # turn input into dict to pass through request.post() as json
-#            # conversion between dict and json is automatic
-#            to_send  = {"entry_string": to_send}
-#            c.send_recieve(to_send)
-#    elif options == "3":
-#        c.read_last()
-#    elif options == "0":
-#        quit()
-#    else:
-#        print("Option not recognised. Try again.")
-#        main()
 
 
 app = Flask(__name__)
