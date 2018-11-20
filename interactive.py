15d6c14b58b1dd36c642dcd419627f7543c6ef22
Kent Martin <kentaasvang@gmail.com>
2018-11-20 15:53:53 +0100

    hello-world server done

--- interactive.py
+++ interactive.py
@@ -2,6 +2,10 @@ import requests
 import json
 
 
+from flask import Flask, render_template
+
+
+
 MENU_TEXT = """
 
 Choose the following options:
@@ -37,38 +41,47 @@ class Communicator(object):
         print(f"{original_entry} -> {processed_entry}")
 
 
-def main():
-    options = input(MENU_TEXT)
-    c = Communicator()
+#def main():
+#    options = input(MENU_TEXT)
+#    c = Communicator()
+#
+#    if options == "1":
+#        to_send = input("Type what you want to send\n> ")
+#
+#        # turn input into dict to pass through request.post() as json
+#        # conversion between dict and json is automatic 
+#        to_send  = {"entry_string": to_send}
+#        c.send_recieve(to_send)
+#    elif options == "2":
+#        print("Type what you want to send. \nType 'q' to quit.")
+#
+#        while True:
+#            to_send = input("\n> ")
+#
+#            if to_send == "q":
+#                quit()
+#
+#            # turn input into dict to pass through request.post() as json
+#            # conversion between dict and json is automatic
+#            to_send  = {"entry_string": to_send}
+#            c.send_recieve(to_send)
+#    elif options == "3":
+#        c.read_last()
+#    elif options == "0":
+#        quit()
+#    else:
+#        print("Option not recognised. Try again.")
+#        main()
 
-    if options == "1":
-        to_send = input("Type what you want to send\n> ")
 
-        # turn input into dict to pass through request.post() as json
-        # conversion between dict and json is automatic 
-        to_send  = {"entry_string": to_send}
-        c.send_recieve(to_send)
-    elif options == "2":
-        print("Type what you want to send. \nType 'q' to quit.")
-
-        while True:
-            to_send = input("\n> ")
-
-            if to_send == "q":
-                quit()
-
-            # turn input into dict to pass through request.post() as json
-            # conversion between dict and json is automatic
-            to_send  = {"entry_string": to_send}
-            c.send_recieve(to_send)
-    elif options == "3":
-        c.read_last()
-    elif options == "0":
-        quit()
-    else:
-        print("Option not recognised. Try again.")
-        main()
+app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    main()
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+#if __name__ == "__main__":
+    #main()
+
